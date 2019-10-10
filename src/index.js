280120db897299396c605038809bb717f31e8029
Ricardo Cesar Gonzalez Zamora <ricardo.gonzalez@4thsource.com>
2019-10-10 13:02:12 -0500

    handle weeks and season by arguments in cli

--- src/index.js
+++ src/index.js
@@ -10,27 +10,42 @@ const { fetchScores } = require('./scrapper');
 const { extractScoresAndWinners  } = require('./scrapper');
 const cheerio = require('cheerio');
 require('dotenv').config();
+const yargs = require('yargs');
+
+const options = yargs
+ .usage("Usage: This tool scraps the scores of previous weeks in the NFL you can use -w <week> to select a week and -s to select the season")
+ .option("w", { alias: "week", describe: "The week number that want to scrap ", type: "number"  })
+ .option("s", { alias: "season", describe: "The season year that want to scrap ", type: "number" })
+ .argv;
+
+ const arguments = yargs.parse();
+
+ console.log('Arguments :', arguments );
 
 const matchWrapper = '.in-progress-table.section tbody';
 const API = require('./API');
-const week = 3;
+let week = arguments.hasOwnProperty('week') ? arguments.week : 1;
+let season = arguments.hasOwnProperty('season') ? arguments.season : 2019;
+week = week > 17 ? 17 : week;
+season = season < 1970 ? 2019 : season;
+
 
 const init = async () => {
     console.log('Initialize app...');
-    const $ = await fetchScores(2019, week);
+    const $ = await fetchScores(season, week);
     const matchesList = $( matchWrapper ); 
     console.log(matchesList.length, 'matches has been found!');
     extractScoresAndWinners( matchesList, week )
     .then( result => {
-        console.log('Extracted Data Correctly! ');
+        console.log('Extracted Data Correctly! ', result );
         let storeData = { matches: result.data, weekId: result.data[0].weekId };
 
         API.storeScores( storeData )
-        .then( response => {
-            console.log( response );
-        })
-        .catch(error => {
-            console.error( error );
+         .then( response => {
+             console.log( response );
+         })
+         .catch(error => {
+             console.error( error );
         })
     })
     .catch(error => {
