7f14f40eea2ac998dd524426b1b58454b2c5fc84
Ricardo Cesar Gonzalez Zamora <ricardo.gonzalez@4thsource.com>
2019-10-10 13:03:17 -0500

    add support to handle static files and render the front end

--- src/server/index.js
+++ src/server/index.js
@@ -1,42 +1,83 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const Matches = require('./models/Matches'); 
-const mongoOptions = {  useNewUrlParser: true ,  useUnifiedTopology: true };
+const Path = require('path');
+const Matches = require('./models/Matches');
+const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 const hapi = require('hapi');
-
-const server =hapi.server({ port: 4000, host: 'localhost'});
-console.log('ENV VARIABLES ', process.env.DB_USER)
-mongoose.connect( `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds333238.mlab.com:33238/scores_nfl`,  mongoOptions );
+const Inert = require('inert');
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds333238.mlab.com:33238/scores_nfl`, mongoOptions);
 mongoose.connection.once('open', () => {
     console.log('connected to db!');
 });
-const init = async () => {
- server.route( [
-    {
-        method: 'GET',
-        path:'/',
-        handler: function(reques, reply){
-            return '<h1>My home Route</h1>'
-        }
-    },
-     {
-         method: 'POST',
-         path: '/api/v1/matches',
-         handler: ( req, reply ) => {
-            console.log('Post matches into server');
-            const { matches, weekId } = req.payload;
-            console.log('matches',matches)
-            console.log('weekId',weekId)
-            const storeMatches = new Matches({
-                matches, weekId
-            });
-            return storeMatches.save();
-         }
-     }
-  ] );
 
-  await server.start();
-  console.log('Server running @ ---->',server.info.uri );
+
+const init = async() => {
+
+
+    const server = hapi.server({
+        
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, '../../public')
+            }
+        },
+        port: 4000,
+        host: 'localhost'
+    });
+
+    console.log(__dirname)
+
+    await server.register(Inert);
+
+    server.route([
+        {
+            method: 'GET',
+            path: '/{param*}',
+            handler: {
+                directory: {
+                    path: '.',
+                    redirectToSlash: true
+                }
+            }
+        },
+        {
+            config: {
+                cors: {
+                    origin: ['*'],
+                    additionalHeaders: ['cache-control', 'x-requested-with']
+                }
+            },
+            method: 'GET',
+            path: '/api/v1/matches',
+            handler: function(req, reply) {
+                return Matches.find()
+            }
+        },
+        {
+            method: 'POST',
+            path: '/api/v1/matches',
+            handler: (req, reply) => {
+                try{
+                    console.log('Post matches into server');
+                    const { matches, weekId } = req.payload;
+                    console.log('matches', matches)
+                    console.log('weekId', weekId)
+                    const storeMatches = new Matches({
+                        matches,
+                        weekId
+                    });
+                    return storeMatches.save();
+                }
+                catch {
+                    return new Error( 'Something went wrong with the request' )
+                }
+                
+            }
+        }
+    ]);
+
+    await server.start();
+    console.log('Server running @ ---->', server.info.uri);
 }
 
 init();
